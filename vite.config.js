0485bdbd7568be3f06a2c701e723f17564c0d387
Borja Gonzalez <borja@bgon.es>
2021-02-25 18:46:54 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+const path = require('path')
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles/components'),
+      '@utils': path.resolve(__dirname, 'src/utils')
+    }
+  }
+})
