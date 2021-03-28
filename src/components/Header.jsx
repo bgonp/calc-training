420b47853bd349a3c0e25ba9a6fdffe40eab17f4
Borja Gonzalez <borja@bgon.es>
2021-03-28 22:03:29 +0200

    Using modal when logging out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react'
+
 import Button from '@components/Button'
 import LogInIcon from '@components/LogInIcon'
 import LogOutIcon from '@components/LogOutIcon'
+import Modal from '@components/Modal'
+import { useFirebaseContext } from '@contexts/FirebaseContext'
+import { useModal } from '@contexts/ModalContext'
 
 import styles from '@styles/components/Header.module.css'
-import { useFirebaseContext } from '../contexts/FirebaseContext'
 
 const Header = () => {
   const { user, signIn, signOut } = useFirebaseContext()
+  const { renderModal } = useModal()
+
+  const handleSignOut = () => renderModal('Do you want to sign out?', signOut)
+
   return (
     <header className={styles.header}>
       {
         user
-          ? <Button secondary onClick={signOut}><LogOutIcon /></Button>
+          ? <Button secondary onClick={handleSignOut}><LogOutIcon /></Button>
           : <Button primary onClick={signIn}><LogInIcon /></Button>
       }
     </header>
