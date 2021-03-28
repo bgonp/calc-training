e9d7e3f6cb812cb4584f5314fc937360da160826
Borja Gonzalez <borja@bgon.es>
2021-03-28 01:00:13 +0100

    After an attempt, ask if user was right

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,54 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { DrawContext } from 'react-drawarea'
 
 import Button from '@components/Button'
+import CloseIcon from '@components/CloseIcon'
 import RestartIcon from '@components/RestartIcon'
+import TickIcon from '@components/TickIcon'
 import UndoIcon from '@components/UndoIcon'
+import { useFirebaseContext } from '@contexts/FirebaseContext'
+
 import styles from '@styles/components/Footer.module.css'
 
 const Footer = ({ solved, handleRestart, handleSolve }) => {
+  const [answered, setAnswered] = useState(false)
   const { undo, reset } = useContext(DrawContext)
+  const { setSuccess } = useFirebaseContext()
 
   const onRestart = () => {
+    setAnswered(false)
     handleRestart()
     reset()
   }
 
+  const handleAnswer = (success) => () => {
+    setSuccess(success)
+    setAnswered(true)
+  }
+
+  const startedButtons = (
+    <>
+      <Button grow secondary onClick={handleSolve}>SOLVE</Button>
+      <Button primary onClick={undo}><UndoIcon /></Button>
+    </>
+  )
+
+  const answerButtons = (
+    <>
+      <Button grow primary onClick={handleAnswer(true)}><TickIcon /></Button>
+      <Button grow secondary onClick={handleAnswer(false)}><CloseIcon /></Button>
+    </>
+  )
+
+  const finishedButtons = (
+    <Button grow primary onClick={onRestart}>
+      <RestartIcon />
+    </Button>
+  )
+
   return (
     <div className={styles.footer}>
-      {
-        solved
-          ? <Button grow primary onClick={onRestart}><RestartIcon /></Button>
-          : (
-            <>
-              <Button grow secondary onClick={handleSolve}>SOLVE</Button>
-              <Button primary onClick={undo}><UndoIcon /></Button>
-            </>
-            )
-      }
+      {solved ? (answered ? finishedButtons : answerButtons) : startedButtons}
     </div>
   )
 }
