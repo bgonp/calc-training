b90fe2c5cb28b596e6069a5a1cdafb67949f2b9f
Borja Gonzalez <borja@bgon.es>
2021-03-28 22:01:57 +0200

    ModalContext

---
+++ src/contexts/ModalContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from 'react'
+
+import Modal from '@components/Modal'
+
+const ModalContext = createContext()
+
+const initialModalData = {
+  visible: false,
+  message: '',
+  onConfirm: () => {}
+}
+
+export const useModal = () => {
+  const context = useContext(ModalContext)
+  if (!context) throw Error('Cannot use context outside of provider')
+  return context
+}
+
+export const ModalProvider = ({ children }) => {
+  const [modalData, setModalData] = useState(initialModalData)
+  const { visible, message, onConfirm } = modalData
+
+  const handleClose = () => setModalData(initialModalData)
+
+  const handleConfirm = () => {
+    onConfirm()
+    setModalData(initialModalData)
+  }
+
+  const renderModal = (message, onConfirm = () => {}) => {
+    setModalData({
+      visible: true,
+      message,
+      onConfirm
+    })
+  }
+
+  return (
+    <ModalContext.Provider value={{ renderModal }}>
+      {children}
+      {visible &&
+        <Modal message={message} onClose={handleClose} onConfirm={handleConfirm} />}
+    </ModalContext.Provider>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import ReactDOM from 'react-dom'
 
 import App from '@components/App'
 import { FirebaseProvider } from '@contexts/FirebaseContext'
+import { ModalProvider } from '@contexts/ModalContext'
 
 import '@styles/index.css'
 
 ReactDOM.render(
-  <FirebaseProvider>
-    <App />
-  </FirebaseProvider>,
+  <ModalProvider>
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
+  </ModalProvider>,
   document.getElementById('root')
 )
