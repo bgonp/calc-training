2aaabe07942897307c7cc35bafb38619e8c44c28
Borja Gonzalez <borja@bgon.es>
2021-03-28 20:17:23 +0200

    Use right firebase imports

--- src/contexts/FirebaseContext.jsx
+++ src/contexts/FirebaseContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useCallback, useContext, useEffect, useState } from 'react'
-import firebase from 'firebase'
-
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
 firebase.initializeApp({
