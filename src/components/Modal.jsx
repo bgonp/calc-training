07284eb76f8d702145cedd4b667a24eaa667807f
Borja Gonzalez <borja@bgon.es>
2021-03-31 01:27:41 +0200

    Modal no longer needs height

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,17 @@
 
 import Button from '@components/Button'
 import { CloseIcon, TickIcon } from '@components/icons'
-import useHeight from '@hooks/useHeight'
 
 import styles from '@styles/components/Modal.module.css'
 
-const Modal = ({ message, onClose, onConfirm }) => {
-  const height = useHeight()
-
-  return (
-    <div className={styles.modal} style={{ height }}>
-      <div className={styles.content}><p>{message}</p></div>
-      <div className={styles.buttons}>
-        <Button grow primary onClick={onConfirm}><TickIcon /></Button>
-        <Button grow secondary onClick={onClose}><CloseIcon /></Button>
-      </div>
+const Modal = ({ message, onClose, onConfirm }) => (
+  <div className={styles.modal}>
+    <div className={styles.content}><p>{message}</p></div>
+    <div className={styles.buttons}>
+      <Button grow primary onClick={onConfirm}><TickIcon /></Button>
+      <Button grow secondary onClick={onClose}><CloseIcon /></Button>
     </div>
-  )
-}
+  </div>
+)
 
 export default Modal
