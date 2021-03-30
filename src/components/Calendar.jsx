9cf5757b6e5fda1c570457aa21d07e3c0a2abd9d
Borja Gonzalez <borja@bgon.es>
2021-03-31 01:29:18 +0200

    Calendar component

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,26 @@
+import styles from '@styles/components/Calendar.module.css'
+
+const weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+const Stats = ({ data, days, firstDayOfWeek }) => (
+  <div className={styles.calendar}>
+    {weekDays.map(day => <span key={day} className={styles.weekDay}>{day}</span>)}
+    {days.map(date => {
+      const day = parseInt(date.split('-')[2])
+      const dayData = data[date] && `${data[date][1]} / ${data[date][0]}`
+
+      return (
+        <div
+          key={date}
+          className={styles[dayData ? 'filled' : 'empty']}
+          style={day === 1 ? { gridColumnStart: firstDayOfWeek } : {}}
+        >
+          <span className={styles.dayNumber}>{day}</span>
+          {dayData && <span className={styles.dayData}>{dayData}</span>}
+        </div>
+      )
+    })}
+  </div>
+)
+
+export default Stats
