237d9cbb5e3a5a7c826c26b4618af75b2b535ad6
Borja Gonzalez <borja@bgon.es>
2021-03-31 01:19:03 +0200

    normalizeAttemps firebase response

--- src/utils/normalizeAttempts.js
+++ src/utils/normalizeAttempts.js
@@ -1,13 +1,17 @@
+const parseDate = (date) => date.toDate().toISOString().split('T')[0]
+
 export const normalizeAttempts = (snapshot) => {
   const attempts = {}
   snapshot.forEach(document => {
     const data = document.data()
     if (!data.end) return
 
-    const date = data.start.toDate().toISOString().split('T')[0]
-    if (!attempts[date]) attempts[date] = [0, 0]
+    const date = parseDate(data.end)
+    if (!attempts[date]) attempts[date] = [0, 0, 0]
 
-    attempts[date][data.success ? 0 : 1]++
+    attempts[date][0]++
+    if (data.success === true) attempts[date][1]++
+    else if (data.success === false) attempts[date][2]++
   })
 
   return attempts
