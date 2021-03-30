2066238c6f525ef65b736358dca7600211536e8e
Borja Gonzalez <borja@bgon.es>
2021-03-31 01:23:00 +0200

    Left and Right icons components

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,17 +1,21 @@
 import ChartIcon from './ChartIcon'
 import CloseIcon from './CloseIcon'
+import LeftIcon from './LeftIcon'
 import LogInIcon from './LogInIcon'
 import LogOutIcon from './LogOutIcon'
 import RestartIcon from './RestartIcon'
+import RightIcon from './RightIcon'
 import TickIcon from './TickIcon'
 import UndoIcon from './UndoIcon'
 
 export {
   ChartIcon,
   CloseIcon,
+  LeftIcon,
   LogInIcon,
   LogOutIcon,
   RestartIcon,
+  RightIcon,
   TickIcon,
   UndoIcon
 }
