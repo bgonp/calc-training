cba0ef49144568730d734260058c306d918f14b0
Borja Gonzalez <borja@bgon.es>
2021-04-09 17:22:01 +0200

    firebase config object

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,14 +2,16 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-firebase.initializeApp({
+const config = {
   apiKey: import.meta.env.VITE_FB_API_KEY,
   authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FB_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FB_SENDER_ID,
   appId: import.meta.env.VITE_FB_APP_ID,
-})
+}
+
+firebase.initializeApp(config)
 
 const auth = firebase.auth()
 const firestore = firebase.firestore()
